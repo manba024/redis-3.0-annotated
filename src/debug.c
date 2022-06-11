d37ea74f45bcbe851bc6dd6eb304e847373bd6ae
Manba024 <1724017340@qq.com>
2022-06-12 01:04:13 +0800

    兼容 m1 mac

--- src/debug.c
+++ src/debug.c
@@ -505,8 +505,11 @@ static void *getMcontextEip(ucontext_t *uc) {
     /* OSX >= 10.6 */
     #if defined(_STRUCT_X86_THREAD_STATE64) && !defined(__i386__)
     return (void*) uc->uc_mcontext->__ss.__rip;
-    #else
+    #elif defined(__i386__)
     return (void*) uc->uc_mcontext->__ss.__eip;
+    #else
+    /* OSX ARM64 */
+    return (void*) arm_thread_state64_get_pc(uc->uc_mcontext->__ss);
     #endif
 #elif defined(__linux__)
     /* Linux */
@@ -574,30 +577,79 @@ void logRegisters(ucontext_t *uc) {
     logStackContent((void**)uc->uc_mcontext->__ss.__rsp);
     #else
     /* OSX x86 */
+    // redisLog(REDIS_WARNING,
+    // "\n"
+    // "EAX:%08lx EBX:%08lx ECX:%08lx EDX:%08lx\n"
+    // "EDI:%08lx ESI:%08lx EBP:%08lx ESP:%08lx\n"
+    // "SS:%08lx  EFL:%08lx EIP:%08lx CS :%08lx\n"
+    // "DS:%08lx  ES:%08lx  FS :%08lx GS :%08lx",
+    //     (unsigned long) uc->uc_mcontext->__ss.__eax,
+    //     (unsigned long) uc->uc_mcontext->__ss.__ebx,
+    //     (unsigned long) uc->uc_mcontext->__ss.__ecx,
+    //     (unsigned long) uc->uc_mcontext->__ss.__edx,
+    //     (unsigned long) uc->uc_mcontext->__ss.__edi,
+    //     (unsigned long) uc->uc_mcontext->__ss.__esi,
+    //     (unsigned long) uc->uc_mcontext->__ss.__ebp,
+    //     (unsigned long) uc->uc_mcontext->__ss.__esp,
+    //     (unsigned long) uc->uc_mcontext->__ss.__ss,
+    //     (unsigned long) uc->uc_mcontext->__ss.__eflags,
+    //     (unsigned long) uc->uc_mcontext->__ss.__eip,
+    //     (unsigned long) uc->uc_mcontext->__ss.__cs,
+    //     (unsigned long) uc->uc_mcontext->__ss.__ds,
+    //     (unsigned long) uc->uc_mcontext->__ss.__es,
+    //     (unsigned long) uc->uc_mcontext->__ss.__fs,
+    //     (unsigned long) uc->uc_mcontext->__ss.__gs
+    // );
+    // logStackContent((void**)uc->uc_mcontext->__ss.__esp);
+    // #else 
+    /* OSX ARM64 */
     redisLog(REDIS_WARNING,
     "\n"
-    "EAX:%08lx EBX:%08lx ECX:%08lx EDX:%08lx\n"
-    "EDI:%08lx ESI:%08lx EBP:%08lx ESP:%08lx\n"
-    "SS:%08lx  EFL:%08lx EIP:%08lx CS :%08lx\n"
-    "DS:%08lx  ES:%08lx  FS :%08lx GS :%08lx",
-        (unsigned long) uc->uc_mcontext->__ss.__eax,
-        (unsigned long) uc->uc_mcontext->__ss.__ebx,
-        (unsigned long) uc->uc_mcontext->__ss.__ecx,
-        (unsigned long) uc->uc_mcontext->__ss.__edx,
-        (unsigned long) uc->uc_mcontext->__ss.__edi,
-        (unsigned long) uc->uc_mcontext->__ss.__esi,
-        (unsigned long) uc->uc_mcontext->__ss.__ebp,
-        (unsigned long) uc->uc_mcontext->__ss.__esp,
-        (unsigned long) uc->uc_mcontext->__ss.__ss,
-        (unsigned long) uc->uc_mcontext->__ss.__eflags,
-        (unsigned long) uc->uc_mcontext->__ss.__eip,
-        (unsigned long) uc->uc_mcontext->__ss.__cs,
-        (unsigned long) uc->uc_mcontext->__ss.__ds,
-        (unsigned long) uc->uc_mcontext->__ss.__es,
-        (unsigned long) uc->uc_mcontext->__ss.__fs,
-        (unsigned long) uc->uc_mcontext->__ss.__gs
+    "x0:%016lx x1:%016lx x2:%016lx x3:%016lx\n"
+    "x4:%016lx x5:%016lx x6:%016lx x7:%016lx\n"
+    "x8:%016lx x9:%016lx x10:%016lx x11:%016lx\n"
+    "x12:%016lx x13:%016lx x14:%016lx x15:%016lx\n"
+    "x16:%016lx x17:%016lx x18:%016lx x19:%016lx\n"
+    "x20:%016lx x21:%016lx x22:%016lx x23:%016lx\n"
+    "x24:%016lx x25:%016lx x26:%016lx x27:%016lx\n"
+    "x28:%016lx fp:%016lx lr:%016lx\n"
+    "sp:%016lx pc:%016lx cpsr:%08lx\n",
+        (unsigned long) uc->uc_mcontext->__ss.__x[0],
+        (unsigned long) uc->uc_mcontext->__ss.__x[1],
+        (unsigned long) uc->uc_mcontext->__ss.__x[2],
+        (unsigned long) uc->uc_mcontext->__ss.__x[3],
+        (unsigned long) uc->uc_mcontext->__ss.__x[4],
+        (unsigned long) uc->uc_mcontext->__ss.__x[5],
+        (unsigned long) uc->uc_mcontext->__ss.__x[6],
+        (unsigned long) uc->uc_mcontext->__ss.__x[7],
+        (unsigned long) uc->uc_mcontext->__ss.__x[8],
+        (unsigned long) uc->uc_mcontext->__ss.__x[9],
+        (unsigned long) uc->uc_mcontext->__ss.__x[10],
+        (unsigned long) uc->uc_mcontext->__ss.__x[11],
+        (unsigned long) uc->uc_mcontext->__ss.__x[12],
+        (unsigned long) uc->uc_mcontext->__ss.__x[13],
+        (unsigned long) uc->uc_mcontext->__ss.__x[14],
+        (unsigned long) uc->uc_mcontext->__ss.__x[15],
+        (unsigned long) uc->uc_mcontext->__ss.__x[16],
+        (unsigned long) uc->uc_mcontext->__ss.__x[17],
+        (unsigned long) uc->uc_mcontext->__ss.__x[18],
+        (unsigned long) uc->uc_mcontext->__ss.__x[19],
+        (unsigned long) uc->uc_mcontext->__ss.__x[20],
+        (unsigned long) uc->uc_mcontext->__ss.__x[21],
+        (unsigned long) uc->uc_mcontext->__ss.__x[22],
+        (unsigned long) uc->uc_mcontext->__ss.__x[23],
+        (unsigned long) uc->uc_mcontext->__ss.__x[24],
+        (unsigned long) uc->uc_mcontext->__ss.__x[25],
+        (unsigned long) uc->uc_mcontext->__ss.__x[26],
+        (unsigned long) uc->uc_mcontext->__ss.__x[27],
+        (unsigned long) uc->uc_mcontext->__ss.__x[28],
+        (unsigned long) arm_thread_state64_get_fp(uc->uc_mcontext->__ss),
+        (unsigned long) arm_thread_state64_get_lr(uc->uc_mcontext->__ss),
+        (unsigned long) arm_thread_state64_get_sp(uc->uc_mcontext->__ss),
+        (unsigned long) arm_thread_state64_get_pc(uc->uc_mcontext->__ss),
+        (unsigned long) uc->uc_mcontext->__ss.__cpsr
     );
-    logStackContent((void**)uc->uc_mcontext->__ss.__esp);
+    logStackContent((void**) arm_thread_state64_get_sp(uc->uc_mcontext->__ss));
     #endif
 /* Linux */
 #elif defined(__linux__)
